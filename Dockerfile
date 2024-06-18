d8e4b501f315635e606c04354c382212c4363dc4
Luis Palacios <luis.palacios.derqui@gmail.com>
2024-06-18 17:00:14 +0200

    fix

--- Dockerfile
+++ Dockerfile
@@ -25,11 +25,17 @@ RUN apt-get install -y --no-install-recommends \
 # Save the version in an environment file
 RUN echo "${RUNNER_VERSION}" > /runner_version.env
 
-# # cd into the user directory, download and unzip the github actions runner
-# # && echo "${RUNNER_HASH} actions-runner-linux-${RUNNER_PLATFORM}-${RUNNER_VERSION}.tar.gz" | shasum -a 256 -c \
-# RUN cd /home/docker && mkdir actions-runner && cd actions-runner \
-#     && curl -O -L https://github.com/actions/runner/releases/download/v${RUNNER_VERSION}/actions-runner-linux-${RUNNER_PLATFORM}-${RUNNER_VERSION}.tar.gz \
-#     && tar xzf ./actions-runner-linux-${RUNNER_PLATFORM}-${RUNNER_VERSION}.tar.gz
+# cd into the user directory, download and unzip the github actions runner
+# && echo "${RUNNER_HASH} actions-runner-linux-${RUNNER_PLATFORM}-${RUNNER_VERSION}.tar.gz" | shasum -a 256 -c \
+RUN cd /home/docker && mkdir actions-runner-linux-x64 && cd actions-runner-linux-x64 \
+    && curl -O -L https://github.com/actions/runner/releases/download/v${RUNNER_VERSION}/actions-runner-linux-x64-${RUNNER_VERSION}.tar.gz \
+    && tar xzf ./actions-runner-linux-x64-${RUNNER_VERSION}.tar.gz
+RUN cd /home/docker && mkdir actions-runner-linux-arm && cd actions-runner-linux-arm \
+    && curl -O -L https://github.com/actions/runner/releases/download/v${RUNNER_VERSION}/actions-runner-linux-arm-${RUNNER_VERSION}.tar.gz \
+    && tar xzf ./actions-runner-linux-arm-${RUNNER_VERSION}.tar.gz
+RUN cd /home/docker && mkdir actions-runner-linux-arm64 && cd actions-runner-linux-arm64 \
+    && curl -O -L https://github.com/actions/runner/releases/download/v${RUNNER_VERSION}/actions-runner-linux-arm64-${RUNNER_VERSION}.tar.gz \
+    && tar xzf ./actions-runner-linux-arm64-${RUNNER_VERSION}.tar.gz
 
 # # install some additional dependencies
 # RUN chown -R docker ~docker && /home/docker/actions-runner/bin/installdependencies.sh
--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -x
-
 # Expect version number under $1
 RUNNER_VERSION=`cat /runner_version.env`
 echo "RUNNER VERSION: ${RUNNER_VERSION}"
@@ -13,37 +11,38 @@ GH_TOKEN=$GH_TOKEN
 
 # Setup the runner if not already done
 # && echo "${RUNNER_HASH} actions-runner-linux-${RUNNER_PLATFORM}-${RUNNER_VERSION}.tar.gz" | shasum -a 256 -c \
-if [ ! -d /home/docker/actions-runner ]; then
-  mkdir -p /home/docker/actions-runner-linux-x64
-  mkdir -p /home/docker/actions-runner-linux-arm
-  mkdir -p /home/docker/actions-runner-linux-arm64
+# if [ ! -d /home/docker/actions-runner ]; then
+#   mkdir -p /home/docker/actions-runner-linux-x64
+#   mkdir -p /home/docker/actions-runner-linux-arm
+#   mkdir -p /home/docker/actions-runner-linux-arm64
 
-  cd /home/docker/actions-runner-linux-x64
-  curl -O -L https://github.com/actions/runner/releases/download/v${RUNNER_VERSION}/actions-runner-linux-x64-${RUNNER_VERSION}.tar.gz \
-      && tar xzf ./actions-runner-linux-x64-${RUNNER_VERSION}.tar.gz
+#   cd /home/docker/actions-runner-linux-x64
+#   curl -O -L https://github.com/actions/runner/releases/download/v${RUNNER_VERSION}/actions-runner-linux-x64-${RUNNER_VERSION}.tar.gz \
+#       && tar xzf ./actions-runner-linux-x64-${RUNNER_VERSION}.tar.gz
 
-  cd /home/docker/actions-runner-linux-arm
-  curl -O -L https://github.com/actions/runner/releases/download/v${RUNNER_VERSION}/actions-runner-linux-arm-${RUNNER_VERSION}.tar.gz \
-      && tar xzf ./actions-runner-linux-arm-${RUNNER_VERSION}.tar.gz
+#   cd /home/docker/actions-runner-linux-arm
+#   curl -O -L https://github.com/actions/runner/releases/download/v${RUNNER_VERSION}/actions-runner-linux-arm-${RUNNER_VERSION}.tar.gz \
+#       && tar xzf ./actions-runner-linux-arm-${RUNNER_VERSION}.tar.gz
 
-  cd /home/docker/actions-runner-linux-arm64
-  curl -O -L https://github.com/actions/runner/releases/download/v${RUNNER_VERSION}/actions-runner-linux-arm64-${RUNNER_VERSION}.tar.gz \
-      && tar xzf ./actions-runner-linux-arm64-${RUNNER_VERSION}.tar.gz
+#   cd /home/docker/actions-runner-linux-arm64
+#   curl -O -L https://github.com/actions/runner/releases/download/v${RUNNER_VERSION}/actions-runner-linux-arm64-${RUNNER_VERSION}.tar.gz \
+#       && tar xzf ./actions-runner-linux-arm64-${RUNNER_VERSION}.tar.gz
 
-  cd /home/docker
-  PLATFORM=`uname -m`
-  echo PLATFORM="$PLATFORM"
-  if [ "${PLATFORM}" = "aarch" ]; then
-    ln -s actions-runner-linux-arm actions-runner
-  elif [ "${PLATFORM}" = "aarch64" ]; then
-    ln -s actions-runner-linux-arm64 actions-runner
-  else
-    ln -s actions-runner-linux-x64 actions-runner
-  fi
-
-  # install some additional dependencies
-  chown -R docker ~docker && /home/docker/actions-runner/bin/installdependencies.sh
+cd /home/docker
+PLATFORM=`uname -m`
+echo PLATFORM="$PLATFORM"
+if [ "${PLATFORM}" = "aarch" ]; then
+  ln -s actions-runner-linux-arm actions-runner
+elif [ "${PLATFORM}" = "aarch64" ]; then
+  ln -s actions-runner-linux-arm64 actions-runner
+else
+  ln -s actions-runner-linux-x64 actions-runner
 fi
+
+# install some additional dependencies
+chown -R docker ~docker && /home/docker/actions-runner/bin/installdependencies.sh
+
+# fi
 
 
 # Go for the runner
